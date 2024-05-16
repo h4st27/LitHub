662e0f471aaf9052cd42d0c42d827b75746b7222
h4st27 <a.o.mikhalchenko@gmail.com>
2024-05-16 15:07:44 +0300

    lr4

--- Program.cs
+++ Program.cs
@@ -1,67 +1,39 @@
-﻿class Program
+﻿using MyApp.Services;
+using System.Reflection;
+
+class Program
 {
     static void Main(string[] args)
     {
-        ThreadDemo();
-        AsyncAwaitDemo();
-        GetDataFromApiAsyncDemo();
-    }
-    static void ThreadDemo()
-    {
-        Console.WriteLine("ThreadDemo is on stack");
+        Film filmBarbie = new(1, "Barbie", true, 9, 'A');
+        Film filmOppenheimer = new(2, "Oppenheimer", false, 9, 'A');
 
-        Thread thread1 = new Thread(() =>
+        Type filmType = typeof(Film);
+        TypeInfo filmTypeInfo = filmType.GetTypeInfo();
+
+        Console.WriteLine($"Type Name: {filmType.Name}");
+
+        Console.WriteLine("\nMembers:");
+        foreach (MemberInfo member in filmTypeInfo.GetMembers())
         {
-            Thread.Sleep(2000);
-            Console.WriteLine("Thread first closed (2000ms sleep)");
-        });
-
-        Thread thread2 = new Thread(() =>
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine("Thread first closed (1000ms sleep)");
-        });
-
-        thread1.Start();
-        thread2.Start();
-
-        Console.WriteLine("ThreadDemo is out of stack");
-    }
-    static async void AsyncAwaitDemo()
-    {
-        Console.WriteLine("AsyncAwaitDemo is on stack");
-
-        await Task.Run(async () =>
-        {
-            Console.WriteLine("Start of first async operation (wait 3000ms)");
-            await Task.Delay(3000);
-            Console.WriteLine("End of first async operation");
-        });
-        await Task.Run(async () =>
-        {
-            Console.WriteLine("Start of second async operation (wait 2000ms)");
-            await Task.Delay(2000);
-            Console.WriteLine("End of second async operation");
-        });
-        Console.WriteLine("AsyncAwaitDemo is out of stack");
-    }
-    static async Task GetDataFromApiAsyncDemo()
-    {
-        Console.WriteLine("GetDataFromApiAsyncDemo is on stack");
-
-        HttpClient client = new HttpClient();
-        string apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
-        HttpResponseMessage response = await client.GetAsync(apiUrl);
-
-        if (response.IsSuccessStatusCode)
-        {
-            string data = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"Data: {data}");
+            Console.WriteLine($"Name: {member.Name}({member.ReflectedType}), {member.MemberType} ");
         }
-        else
+
+        Console.WriteLine("\nFields:");
+        foreach (FieldInfo fieldInfo in filmTypeInfo.DeclaredFields)
         {
-            Console.WriteLine("Error");
+            Console.WriteLine($"Name: {fieldInfo.Name}({fieldInfo.Attributes}), FieldType: {fieldInfo.FieldType}");
         }
-        Console.WriteLine("GetDataFromApiAsyncDemo is out of stack");
+        Console.WriteLine("\nMethods:");
+        foreach (MethodInfo methodInfo in filmTypeInfo.DeclaredMethods)
+        {
+            Console.WriteLine($"Name: {methodInfo.Name}({methodInfo.Attributes}), MethodType: {methodInfo.ReturnType}");
+        }
+
+       Console.WriteLine("\nReflection:");
+       MethodInfo reflectionMethod = filmType.GetMethod("WriteInfo");
+        if (reflectionMethod != null ) {
+            reflectionMethod.Invoke(filmBarbie, [filmOppenheimer]);
+        }
     }
 }
---
+++ Services/Film.cs
@@ -0,0 +1,36 @@
+﻿
+namespace MyApp.Services
+{
+    internal class Film
+    {
+        private int id;
+        public string name;
+        internal bool isWatched;
+        protected double rating;
+        protected internal char letterRating;
+        public Film(int id, string name, bool isWatched, double rating, char letterRating)
+        {
+            this.id = id;
+            this.name = name;
+            this.isWatched = isWatched;
+            this.rating = rating;
+            this.letterRating = letterRating;
+        }
+        public void WriteFullRating()
+        {
+            Console.WriteLine($"This movies has {rating} points and totally {letterRating} rating");
+        }
+
+        protected int GetId()
+        {
+            return id;
+        }
+
+        public void WriteInfo(Film film)
+        {
+            string isWatchedMessage = isWatched ? "is watched" : "is not watched";
+            string preferToWatchMessage = isWatched ? "watch next one" : "watch it at any time tou want";
+            Console.WriteLine($"{name} {isWatchedMessage}, {preferToWatchMessage}. You also can watch {film.name} ");
+        }
+    }
+}
